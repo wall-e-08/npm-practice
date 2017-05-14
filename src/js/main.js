8e8d1a3ebe9b5ef66c314292e806a64f5747022e
Debashis Roy Bhowmik <debashis.buet08@gmail.com>
2017-05-14 17:39:54 +0600

    replace animateSlider with fractionSlider(working without error) and removed unnecessary bootstrap codes

--- src/js/main.js
+++ src/js/main.js
@@ -37,73 +37,22 @@
 
 	});
 
-	$(document).ready(function () {
-		//jquery slider init (this should be initialize at end)
+	$(window).load(function () {
+
+		$('.slider').fractionSlider({
+			'fullWidth': 			true,
+			'controls': 			false,
+			'pager': 				true,
+			'responsive': 			true,
+			'dimensions': 			"1000,400",
+			'increase': 			false,
+			'pauseOnHover': 		true
+		});
 
 	});
 
 
 
 
-	$(".anim-slider").animateSlider({
-		autoplay    :   true,   //starts the autoplay
-		interval    :   4000,   //time between slides if autoplay is true
-		animations  :           //specify the animations for each element of the slide
-			{
-				0 :
-					{
-						h1 : {   //tagName or id or class name of the element
-							show: "bounceInUp",   //class to add when the element appears
-							hide: "flipOutX",  //class to add when the element disappears
-							delayShow: "delay0-5s"   //class to add to delay show effect
-						},
-						h3 : {
-							show: "bounceIn",
-							hide: "bounceOut",
-							delayShow: "delay1s"
-						},
-						ul : {
-							show: "rotateIn",
-							hide: "rotateOut",
-							delayShow: "delay1-5s"
-						}
-					},
-				1 :
-					{
-						".xaxa1": {
-							show: "slideInRight",
-							hide: "slideInOut",
-							delayShow: "delay0-5s"
-						},
-						"#test1": {
-							show: "slideInLeft",
-							hide: "slideOutRight",
-							delayShow: "delay1s"
-						},
-						".offers1": {
-							show: "fadeIn",
-							hide: "fadeOut",
-							delayShow: "delay1-5s"
-						}
-					},
-				2 :
-					{
-						".xaxa2": {
-							show: "shakeX",
-							hide: "shakeX",
-							delayShow: "delay0-5s"
-						},
-						"#test2": {
-							show: "rotateInUpLeft",
-							hide: "rotateOutUpLeft",
-							delayShow: "delay1s"
-						},
-						".offers2": {
-							show: "rotateIn",
-							hide: "rotateOut",
-							delayShow: "delay1-5s"
-						}
-					}
-			}
-	});
+
 })(jQuery);
