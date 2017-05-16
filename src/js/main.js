f227ec577179db2dfb3f2f6a963229a492682141
Debashis Roy Bhowmik <debashis.buet08@gmail.com>
2017-05-16 18:09:41 +0600

    added logo change functionality

--- src/js/main.js
+++ src/js/main.js
@@ -12,12 +12,32 @@
 			else
 				_themeSelector.addClass('colorbox-open');
 		});
-
-
 		$('.all-colors>button.btn').click(function (e) {
 			e.preventDefault();
 			var _color = $(this).attr('data-color');
 			$('body').removeAttr('id').attr('id', _color);
+		});
+
+
+
+		//open logo box to change logo
+		$('.change-logo').click(function (e) {
+			e.preventDefault();
+			var _logoSelector = $('#logo-changer');
+			if (_logoSelector.hasClass('logobox-open'))
+				_logoSelector.removeClass('logobox-open');
+			else
+				_logoSelector.addClass('logobox-open');
+		});
+		$('.all-logos>button.btn').click(function (e) {
+			e.preventDefault();
+			//var _color = $(this).attr('data-color');
+			//$('body').removeAttr('id').attr('id', _color);
+			var _this = $(this);
+			var _index = _this.index() + 1;
+
+			_this.children('img').removeAttr('src').attr('src','img/logo/logo-' + _index + '.png');
+			$('.favicon').removeAttr('href').attr('href','img/favicon/fav-' + _index + '.png');
 		});
 
 
